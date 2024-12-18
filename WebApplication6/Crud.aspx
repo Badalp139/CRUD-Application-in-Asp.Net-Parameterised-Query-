a7ab9e173bcc1096fe3938b2e7f9759f4c31e89e
Badalp139 <pradhanjb139@gmail.com>
2024-12-18 20:15:56 +0530

    Add project files.

---
+++ WebApplication6/Crud.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Crud.aspx.cs" Inherits="WebApplication6.Crud" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            height: 31px;
+        }
+    </style>
+</head>
+<body>
+    <h1 align="center">Crud Application in Asp.Net (Parameterised)</h1>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td>Employe Id</td>
+                    <td>
+                        <asp:TextBox ID="txtEmpId" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>Employe Name</td>
+                    <td>
+                        <asp:TextBox ID="txtEmpName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>Salary</td>
+                    <td>
+                        <asp:TextBox ID="txtSalary" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>City</td>
+                    <td>
+                        <asp:TextBox ID="txtCity" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style2"></td>
+                    <td class="auto-style2">
+                        <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" />
+&nbsp;&nbsp;
+                        <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+&nbsp;&nbsp;
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+&nbsp;&nbsp;
+                        <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>
+                        <asp:Label ID="lblMessage" runat="server" Text="Label"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication6/Crud.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication6
+{
+    public partial class Crud : System.Web.UI.Page
+    {
+        SqlConnection cn = new SqlConnection("data source=LAPTOP-P4KQMG8A\\SQLEXPRESS07;initial catalog=demo;integrated security=true");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn.Open();
+                string query = "insert into employe2(empid,name,salary,city)values(@empid,@name,@salary,@city)";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.Parameters.AddWithValue("@empid", txtEmpId.Text);
+                cmd.Parameters.AddWithValue("@name", txtEmpName.Text);
+                cmd.Parameters.AddWithValue("@salary", txtSalary.Text);
+                cmd.Parameters.AddWithValue("@city", txtCity.Text);
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    lblMessage.Text = "Record Update sucessful";
+                }
+                else
+                {
+                    lblMessage.Text = "record not inserted";
+                    cn.Close();
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn.Open();
+                string query = "update employe2 set salary=@salary,empid=@empid,name=@name,city=@city where empid=@empid";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.Parameters.AddWithValue("@salary", txtSalary.Text);
+                cmd.Parameters.AddWithValue("@name", txtEmpName.Text);
+                cmd.Parameters.AddWithValue("@City", txtCity.Text);
+                cmd.Parameters.AddWithValue("@empid", txtEmpId.Text);
+                int result = cmd.ExecuteNonQuery();
+                if(result==1)
+                {
+                    lblMessage.Text = "Record Update Sucessfully";
+                }
+                else
+                {
+                    lblMessage.Text = "Record Not Update";
+                    cn.Close();
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn.Open();
+                string query = "delete from employe2 where empid=@empid";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.Parameters.AddWithValue("@empid", txtEmpId.Text);
+                int result = cmd.ExecuteNonQuery();
+                if(result==1)
+                {
+                    lblMessage.Text = "Record Delete sucessful";
+                }
+                else
+                {
+                    lblMessage.Text = "Record Not Delete";
+                    cn.Close();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtEmpName.Text = string.Empty;
+            txtCity.Text = string.Empty;
+            txtEmpId.Text = string.Empty;
+            txtSalary.Text = string.Empty;
+            lblMessage.Text = string.Empty;
+        }
+    }
+}
